e5da37546fc1f635f74e67d135010a0e03f25ca0
Suado Cowboy <luccacsg@gmail.com>
2024-07-02 18:11:33 -0300

    tried implementing tick system

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <enet/enet.h>
+#include <chrono>
 
 namespace rl {
     #include <raylib.h>
@@ -9,6 +10,7 @@ namespace rl {
 #include "Player.h"
 #include "Packet.h"
 #include "ClientsHandler.h"
+#include "TickHandler.h"
 #include "Shared.h"
 
 #define MAX_CLIENTS 32
@@ -45,19 +47,29 @@ int main() {
         std::cout << "Address: " << ip << ":" << host->address.port << "\n";
     }
 
+    TickHandler tickHandler(0.015);
+
+    auto dtStartTime = std::chrono::steady_clock::now();
+
     bool running = true;
     while (running) {
         ENetEvent event;
-        /* Wait up to 1000 milliseconds for an event. (WARNING: blocking) */
-        while (enet_host_service(host, &event, 1000) > 0) {
-            Client* pClient = nullptr;
-            Packet packet;
-            char ip[16];
 
+        tickHandler.update(
+            std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::steady_clock::now() - dtStartTime).count()*0.001f);
+        dtStartTime = std::chrono::steady_clock::now();
+
+        if (!tickHandler.shouldTick()) continue;
+
+        /* Wait up to 1000 milliseconds for an event. (WARNING: blocking) */
+        while (enet_host_service(host, &event, 0) > 0) {
             switch (event.type) {
-                case ENET_EVENT_TYPE_CONNECT:
+                case ENET_EVENT_TYPE_CONNECT: {
+                    char ip[16];
                     enet_address_get_host_ip(&host->address, ip, sizeof(ip));
 
+                    Client* pClient;
                     {
                         Player clientPlayer;
                         clientPlayer.rect = {0,0, 20,20};
@@ -68,6 +80,7 @@ int main() {
 
                     std::cout << "NEW CLIENT => ID: " << (enet_uint16)pClient->id << " | IP: " << ip << ":" << event.peer->address.port << "\n";
 
+                    Packet packet;
                     packet << (enet_uint8)CLIENT_CONNECT
                            << pClient->id
                            << pClient->player.color.r
@@ -76,42 +89,85 @@ int main() {
                            << pClient->player.rect;
                     broadcastPacket(host, packet, true, 0);
                     packet.deleteData();
-
-                    packet << (enet_uint8)CLIENTS_LIST
+                    
+                    packet << (enet_uint8)SERVER_DATA
+                           << tickHandler.tickInterval
                            << clients.size()-1;
                     sendPacket(event.peer, packet, true, 0);
                     packet.deleteData();
 
                     for (auto& client : clients.get()) {
-                        if (client.id == pClient->id)
+                        if (client->id == pClient->id)
                             continue;
 
-                        packet << (enet_uint8)CLIENTS_LIST
-                               << client.id
-                               << client.player.color.r
-                               << client.player.color.g
-                               << client.player.color.b
-                               << client.player.rect;
+                        packet << (enet_uint8)SERVER_DATA
+                               << client->id
+                               << client->player.color.r
+                               << client->player.color.g
+                               << client->player.color.b
+                               << client->player.rect;
                         
                         sendPacket(event.peer, packet, true, 0);
                         packet.deleteData();
                     }
                     break;
+                }
 
-                case ENET_EVENT_TYPE_RECEIVE:
+                case ENET_EVENT_TYPE_RECEIVE: {
+                    PacketUnwrapper packetUnwrapper{event.packet->data};
+
+                    PacketType packetType = NONE;
+                    {
+                        enet_uint8 packetTypeTemp;
+                        packetUnwrapper >> packetTypeTemp;
+                        packetType = (PacketType)packetTypeTemp;
+                    }
+
+                    if (packetType == PLAYER_INPUT) {
+                        Vector2uc playerDir;
+                        packetUnwrapper >> playerDir;
+
+                        if (playerDir.x > 2 || playerDir.y > 2) {
+                            enet_packet_destroy(event.packet);
+                            break;
+                        }
+                        
+                        Client* pClient = clients.getByAddress(event.peer->address);
+                        if (!pClient) {
+                            enet_packet_destroy(event.packet);
+                            break;
+                        }
+
+                        Packet packet;
+                        packet << PLAYER_INPUT
+                               << (enet_uint16)pClient->id
+                               << playerDir
+                               << pClient->player.rect.x
+                               << pClient->player.rect.y;
+                        
+                        broadcastPacket(host, packet, false, 1);
+                        packet.deleteData();
+
+                        pClient->player.dir = playerDir;
+                    }
+
                     enet_packet_destroy(event.packet);
                     break;
+                }
 
-                case ENET_EVENT_TYPE_DISCONNECT:
-                    pClient = clients.getByAddress(event.peer->address);
+                case ENET_EVENT_TYPE_DISCONNECT: {
+                    Client* pClient = clients.getByAddress(event.peer->address);
                     if (!pClient) {
                         std::cout << "UNKNOWN CLIENT TRIED TO DISCONNECT\n";
                         break;
                     }
 
+                    char ip[16];
                     enet_address_get_host_ip(&pClient->address, ip, sizeof(ip));
 
                     std::cout << "CLIENT DISCONNECTED => ID: " << (enet_uint16)pClient->id << " | IP: " << ip << ":" << pClient->address.port << "\n";
+                    
+                    Packet packet;
                     packet << (enet_uint8)CLIENT_DISCONNECT
                            << pClient->id;
 
@@ -121,11 +177,16 @@ int main() {
                     clients.erase(pClient->id);
                     event.peer->data = NULL;
                     break;
+                }
 
                 case ENET_EVENT_TYPE_NONE:
                     break;
             }
         }
+    
+        // TODO: update system with tick system using sleep function
+        for (auto& client : clients.get())
+            client->player.update();
     }
 
     enet_host_destroy(host);
