5837d5943c6f26cdd818e5386f61b0d1b9d8c7e1
Suado Cowboy <luccacsg@gmail.com>
2024-07-02 18:10:50 -0300

    updated too much to comment. Trying to make movement work as it should(not implemented thread yet)

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 #include <enet/enet.h>
+
+#ifdef DrawText
+#undef DrawText
+#endif
+
+#ifdef DrawTextEx
+#undef DrawTextEx
+#endif
 
 namespace rl {
     #include <raylib.h>
 }
 
+#define CLIENT
 #include "Player.h"
 #include "Packet.h"
 #include "Shared.h"
+#include "TickHandler.h"
 
 #define PORT 5055
 
@@ -18,17 +29,20 @@ struct Client {
     Player player;
 };
 
-enet_uint8 clientId;
+Client* pClient = nullptr;
 
-std::vector<Client> clients;
+std::vector<Client*> clients;
 void eraseClient(const enet_uint8 id) {
     enet_uint8 clientsSize = clients.size();
     for (enet_uint8 i = 0; i < clientsSize;) {
-        if (clients[i].id > id)
-            --(clients[i].id);
+        if (clients[i]->id > id) {
+            --(clients[i]->id);
+            clients[i]->player.setIdText(std::to_string(clients[i]->id).c_str(), 10);
+        }
         
-        else if (clients[i].id == id) {
+        else if (clients[i]->id == id) {
             --clientsSize;
+            delete clients[i];
             clients.erase(clients.begin()+i);
             continue;
         }
@@ -36,23 +50,24 @@ void eraseClient(const enet_uint8 id) {
         ++i;
     }
 
-    if (clientId > id) --clientId;
+    if (pClient->id > id) --pClient->id;
 }
 
-enet_uint8 addClient(const enet_uint8& id, const Player& player) {
-    Client client = {
-        id,
-        player
-    };
-
-    clients.push_back(client);
+enet_uint8 addClient(const enet_uint8& id, Player& player) {
+    player.setIdText(std::to_string(id).c_str(), 10);
     
-    return client.id;
+    Client* pClient = new Client();
+    pClient->id = id;
+    pClient->player = player;
+
+    clients.push_back(pClient);
+    
+    return pClient->id;
 }
 
 Client* getClientById(const enet_uint8& id) {
     for (auto& client : clients)
-        if (client.id == id) return &client;
+        if (client->id == id) return client;
     
     return nullptr;
 }
@@ -116,6 +131,8 @@ enet_uint8 handleClientConnect(const ENetEvent& event, PacketUnwrapper& packetUn
 }
 
 int main() {
+    rl::InitWindow(1270, 720, "Multiplayer Game");
+
     if (enet_initialize() != 0) {
         std::cerr << "An error occurred while initializing ENet." << std::endl;
         exit(EXIT_FAILURE);
@@ -132,34 +149,35 @@ int main() {
     enet_address_set_host(&address, "127.0.0.1");
     address.port = PORT;
 
-    ENetPeer* peer = enet_host_connect(client, &address, 2, 0);
-    if (peer == NULL) {
+    ENetPeer* serverPeer = enet_host_connect(client, &address, 2, 0);
+    if (serverPeer == NULL) {
         std::cerr << "No available peers for initiating an ENet connection." << std::endl;
         exit(EXIT_FAILURE);
     }
 
     ENetEvent event;
     if (enet_host_service(client, &event, 5000) <= 0 || event.type != ENET_EVENT_TYPE_CONNECT) {
-        enet_peer_reset(peer);
+        enet_peer_reset(serverPeer);
         puts("Connection to server failed.");
         exit(EXIT_FAILURE);
     } else if (event.type == ENET_EVENT_TYPE_RECEIVE)
         enet_packet_destroy(event.packet);
     
+    TickHandler tickHandler;
+
     // Get initial data
     bool handlingClientsList = false;
-    bool definedOwnClient = false;
     enet_uint8 clientsListSize = 0;
     enet_uint8 clientsListIndex = 0;
     while (enet_host_service(client, &event, 5000) > 0 && event.type == ENET_EVENT_TYPE_RECEIVE) {
         if (event.packet->dataLength < sizeof(enet_uint8)) {
             std::cout << "ERROR WHILE RECEIVING PACKET => DATA LENGTH: " << event.packet->dataLength << std::endl;
-            enet_peer_reset(peer);
+            enet_peer_reset(serverPeer);
             enet_packet_destroy(event.packet);
             exit(EXIT_FAILURE);
         }
 
-        PacketUnwrapper packetUnwrapper((const char*)event.packet->data);
+        PacketUnwrapper packetUnwrapper(event.packet->data);
 
         PacketType packetType;
         {
@@ -170,22 +188,25 @@ int main() {
 
         if (packetType == NONE) {
             std::cerr << "ERROR WHILE PARSING PACKET => TYPE IS NONE" << std::endl;
-            enet_peer_reset(peer);
+            enet_peer_reset(serverPeer);
             enet_packet_destroy(event.packet);
             exit(EXIT_FAILURE);
         }
 
         if (packetType == CLIENT_CONNECT) {
             enet_uint8 id = handleClientConnect(event, packetUnwrapper);
-            if (!definedOwnClient)
-                clientId = id;
+            if (!pClient)
+                pClient = getClientById(id);
         }
             
-        else if (packetType == CLIENTS_LIST) {
+        else if (packetType == SERVER_DATA) {
             if (!handlingClientsList) {
                 handlingClientsList = true;
 
-                packetUnwrapper >> clientsListSize;
+                packetUnwrapper >> tickHandler.tickInterval
+                                >> clientsListSize;
+                
+                tickHandler.tickInterval *= 2.0f;
                 if (clientsListSize == 0)
                     break;
                 
@@ -201,18 +222,32 @@ int main() {
         enet_packet_destroy(event.packet);
     }
 
-    std::cout << "PLAYERS: " << clientsListSize+1 << "\n";
+    std::cout << "PLAYERS CONNECTED: " << clientsListSize+1 << "\n";
 
-    while (true) {
-        if (enet_host_service(client, &event, 5000) > 0 && event.type == ENET_EVENT_TYPE_RECEIVE) {
+    while (!rl::WindowShouldClose()) {
+        float dt = rl::GetFrameTime();
+        
+        rl::BeginDrawing();
+        rl::ClearBackground(rl::BLACK);
+
+        for (auto& client : clients) {
+            rl::DrawRectangle(client->player.rect.x, client->player.rect.y, client->player.rect.width, client->player.rect.height, client->player.color);
+            rl::DrawText(client->player.idText.c_str(), client->player.idTextPosition.x, client->player.idTextPosition.y, client->player.idTextFontSize, rl::WHITE);
+        }
+        rl::EndDrawing();
+
+        tickHandler.update(dt);
+        if (!tickHandler.shouldTick()) continue;
+        
+        while (enet_host_service(client, &event, 0) > 0 && event.type == ENET_EVENT_TYPE_RECEIVE) {
             if (event.packet->dataLength < sizeof(enet_uint8)) {
                 std::cout << "ERROR WHILE RECEIVING PACKET => DATA LENGTH: " << event.packet->dataLength << std::endl;
-                enet_peer_reset(peer);
+                enet_peer_reset(serverPeer);
                 enet_packet_destroy(event.packet);
                 exit(EXIT_FAILURE);
             }
             
-            PacketUnwrapper packetUnwrapper((const char*)event.packet->data);
+            PacketUnwrapper packetUnwrapper(event.packet->data);
 
             enet_uint8 type;
             packetUnwrapper >> type;
@@ -223,17 +258,51 @@ int main() {
 
                 eraseClient(id);
                 std::cout << "DISCONNECTED => ID: " << (enet_uint16)id
-                          << "\nCURRENT CLIENT ID: " << (enet_uint16)clientId << "\n";
+                          << "\nCURRENT CLIENT ID: " << (enet_uint16)pClient->id << "\n";
             }
 
             if (type == CLIENT_CONNECT)
                 handleClientConnect(event, packetUnwrapper);
+            
+            if (type == PLAYER_INPUT) {
+                enet_uint8 id;
+                packetUnwrapper >> id;
+                Client* pSomeClient = getClientById(id);
+                if (!pSomeClient) {
+                    std::cout << "ERROR => received input from a client that doesn't seem to exist\n";
+                    enet_packet_destroy(event.packet);
+                    break;
+                }
+
+                {
+                    Vector2uc dir;
+                    packetUnwrapper >> dir;
+                    if (id != pClient->id)
+                        pClient->player.dir = dir;
+                }
+
+                packetUnwrapper >> pSomeClient->player.rect.x
+                                >> pSomeClient->player.rect.y;
+                
+                 std::cout << "INPUT => ID: " << (enet_uint16)id << " | DIR: ("
+                           << (enet_uint16)pSomeClient->player.dir.x << ", " << (enet_uint16)pSomeClient->player.dir.y << ") | POSITION: ("
+                           << pSomeClient->player.rect.x << ", " << pSomeClient->player.rect.y << ")\n";
+            }
 
             enet_packet_destroy(event.packet);
         }
+
+        for (auto& client : clients) {
+            if (client->id == pClient->id)
+                client->player.update(serverPeer);
+            else
+                client->player.update(nullptr);
+        }
     }
 
-    disconnect(client, event, peer);
+    rl::CloseWindow();
+
+    disconnect(client, event, serverPeer);
     enet_host_destroy(client);
     enet_deinitialize();
 }
