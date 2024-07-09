b4756aa7bcfccd2e032883023d3cc44bedfc2be7
Suado Cowboy <luccacsg@gmail.com>
2024-07-09 14:58:59 -0300

    added lerpVec2 and distance2D

--- src/Math.cpp
+++ src/Math.cpp
@@ -1,5 +1,14 @@
-#include "Math.h"
+#include <bits/stdc++.h>
+#include "MMath.h"
 
-float lerp(float start, float end, float amount) {
+float lerp(const float& start, const float& end, const float& amount) {
     return start + (end - start) * amount;
+}
+
+rl::Vector2 lerpVec2(const rl::Vector2& start, const rl::Vector2& end, const float& amount) {
+    return (rl::Vector2){lerp(start.x, end.x, amount), lerp(start.y, end.y, amount)};
+}
+
+double distance2D(const double& x1, const double& y1, const double& x2, const double& y2) {
+    return std::sqrt(std::pow((x2 - x1), 2) + std::pow((y2 - y1), 2));
 }
--- src/Math.h
+++ src/Math.h
@@ -1,3 +1,10 @@
 #pragma once
 
-float lerp(float start, float end, float amount);
+namespace rl {
+    #include <raylib.h>
+}
+
+float lerp(const float& start, const float& end, const float& amount);
+rl::Vector2 lerpVec2(const rl::Vector2& start, const rl::Vector2& end, const float& amount);
+
+double distance2D(const double& x1, const double& y1, const double& x2, const double& y2);
